05d96252c1eed958ed3d3eb686bf31df7ca244d8
washi-denz <washidenz@gmail.com>
2021-04-08 09:33:26 -0500

    Se creó un archivo llamado 'portal' en donde se alojará page principal...,por lo pronto trabajaré en modo crudo pq la funcionalidad es lo más importante

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class VoteController extends Controller
 {
-    //
+    public function index(){
+        return view('portal.index');
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[VoteController::class,'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
