342d570bd8e2861d3bf384893c9f8480721f2548
Josue Mazco Puma <jossmp@gmail.com>
2021-04-09 02:26:48 -0500

    Agregado traducciones a espanol

--- resources/lang/es/pagination.php
+++ resources/lang/es/pagination.php
@@ -1,19 +1,9 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Pagination Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used by the paginator library to build
-    | the simple pagination links. You are free to change them to anything
-    | you want to customize your views to better match your application.
-    |
-    */
-
-    'previous' => '&laquo; Previous',
-    'next' => 'Next &raquo;',
-
+    'reset'     => '¡Su contraseña ha sido restablecida!',
+    'sent'      => '¡Le hemos enviado por correo electrónico el enlace para restablecer su contraseña!',
+    'throttled' => 'Por favor espere antes de intentar de nuevo.',
+    'token'     => 'El token de restablecimiento de contraseña es inválido.',
+    'user'      => 'No encontramos ningún usuario con ese correo electrónico.',
 ];
