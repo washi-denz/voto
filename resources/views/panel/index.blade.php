47ffc4f55b55cf4ed91e6f764e91f4c90c287445
Josue Mazco Puma <jossmp@gmail.com>
2021-04-09 02:28:15 -0500

    Agregado controlador y vista de panel de administracion

---
+++ resources/views/panel/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title','Panel de administracion');
+
+@section('content')
+<div class="py-0">
+    <div class="bg-white border-b border-gray-300">
+        <h4 class="text-2xl text-gray-600">ADMIN - PAGE INDEX</h4>
+    </div>
+</div>
+@endsection
