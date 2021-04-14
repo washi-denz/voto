2c75f9436362ec120b9bfe13cc6c1d6e57d69591
Josue Mazco Puma <jossmp@gmail.com>
2021-04-14 04:43:16 -0500

    Agregado rutas para reporte de votacion

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\CandidateController;
 use App\Http\Controllers\CensusController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VoteController;
-
+use App\Http\Controllers\VotingResultController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +23,7 @@ use App\Http\Controllers\VoteController;
 Route::prefix('panel')->name('panel.')->group(function () {
     Route::resource('/', AdminController::class)->middleware('auth')->only(['index']);
     Route::resource('/census', CensusController::class)->parameters(['census' => 'census'])->middleware('auth');
+    Route::resource('/vote', VotingResultController::class)->middleware('auth');
     Route::post('/census/import', [CensusController::class, 'import_csv'])->middleware('auth')->name('census.import');
     Route::resource('/candidate', CandidateController::class)->middleware('auth');
 
@@ -31,12 +32,12 @@ Route::prefix('panel')->name('panel.')->group(function () {
 
 
 // PORTAL
-Route::get('/', [VoteController::class,'index'])->name('portal.home');
+Route::get('/', [VoteController::class, 'index'])->name('portal.home');
 
-Route::prefix('portal')->name('portal.')->group(function(){
-    Route::resource('/vote',VoteController::class);
-    Route::get('/voto/confirm',[VoteController::class,'show_confirm'])->name('show.confirm');
-    Route::post('/voto/confirm/{id}',[VoteController::class,'update_confirm'])->name('update.confirm');
+Route::prefix('portal')->name('portal.')->group(function () {
+    Route::resource('/vote', VoteController::class);
+    Route::get('/voto/confirm', [VoteController::class, 'show_confirm'])->name('show.confirm');
+    Route::post('/voto/confirm/{id}', [VoteController::class, 'update_confirm'])->name('update.confirm');
 });
 
 /*
